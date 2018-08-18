0954a6581e290a63886050bcfb379976700f9080
andreas <andreas_osby@hotmail.com>
2018-08-18 16:55:05 +0200

    pixel graphics using canvas

--- js/game.js
+++ js/game.js
@@ -1,19 +1,47 @@
 (function(){
     "use strict";
-    let field = document.getElementsByClassName("gamefield")[0];
 
     let NR_ROWS = 20;
     let NR_COLS = 20;
+    let GAMEFIELD = [];
     let ALIVE_V = 1;
     let DEAD_V  = 0;
+    let SIZE = 32; //size of rectangles
+    let BUFFER, CONTEXT;
+    BUFFER = document.createElement("canvas").getContext("2d");
+    CONTEXT = document.querySelector("canvas").getContext("2d");
 
+    BUFFER.canvas.width = NR_ROWS * SIZE;
+    BUFFER.canvas.height = NR_COLS * SIZE;
     /*
     * Start function for game
      */
     function start(){
-        let gamefield = init_game();
-        console.log(gamefield);
+        init_gamefield();
     }
+
+
+
+    /*
+    * Infinity loop the game
+     */
+    function game_loop() {
+        while(true) {
+            console.log(GAMEFIELD);
+            tick();
+            // add sleep
+        }
+    }
+
+
+
+    /*
+    * Calculates a tick.
+    * During a tick, each cell is check against the rules.
+     */
+     function tick() {
+
+     }
 
 
 
@@ -27,16 +55,52 @@
 
 
     /*
-    * Create a 2d array as gamefiled
+    * Create a 2d array as gamefield
      */
-    function init_game(){
-        let gamefield = new Array(NR_ROWS);
+    function init_gamefield(){
+        GAMEFIELD = new Array(NR_ROWS);
         for (let i = 0; i < NR_ROWS; i++) {
-            gamefield[i] = Array.from({length: NR_COLS}, () => randomInt(0, 1));
+            GAMEFIELD[i] = Array.from({length: NR_COLS}, () => randomInt(0, 1));
         }
-        return gamefield;
+
+        resize();
     }
 
 
+
+    /*
+    * Draw canvas gamefield.
+    * Got code from https://github.com/frankarendpoth/frankarendpoth.github.io/blob/master/content/pop-vlog/javascript/2017/012-tile-world/tile-world.js
+     */
+    function draw_gamefield() {
+        for (let i = 0; i < GAMEFIELD.length; i ++) {
+            for (let j = 0; j < GAMEFIELD[i].length; j ++) {
+                BUFFER.fillStyle = (GAMEFIELD[i][j] == 1)?"#000000":"#ffffff";
+                BUFFER.fillRect(i*SIZE, j*SIZE, SIZE, SIZE);
+        }
+      }
+
+      CONTEXT.drawImage(BUFFER.canvas, 0, 0, BUFFER.canvas.width, BUFFER.canvas.height, 0, 0, CONTEXT.canvas.width, CONTEXT.canvas.height);
+    };
+
+
+
+    /*
+    * Resizes gamefield when window changes
+     */
+    function resize(event) {
+
+        CONTEXT.canvas.width = Math.floor(document.documentElement.clientWidth - SIZE);
+
+        if (CONTEXT.canvas.width > document.documentElement.clientHeight) {
+            CONTEXT.canvas.width = Math.floor(document.documentElement.clientHeight);
+        }
+        CONTEXT.canvas.height = Math.floor(CONTEXT.canvas.width * 0.5625);
+
+        draw_gamefield();
+    };
+
+
+    window.addEventListener("resize", resize, {passive:true});
     start();
 })()
