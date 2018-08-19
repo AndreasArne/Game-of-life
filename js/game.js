2fc73c83b33e83ac9d29cabdbab0ead7f3bcdcca
andreas <andreas_osby@hotmail.com>
2018-08-19 17:34:24 +0200

    borken redraw of canvas

--- js/game.js
+++ js/game.js
@@ -18,6 +18,13 @@
      */
     function start(){
         init_gamefield();
+
+        document.querySelector("canvas").onclick= function() {
+            window.requestAnimationFrame(game_loop);
+        };
+        // window.requestAnimationFrame(game_loop);
+
+        // game_loop();
     }
 
 
@@ -25,12 +32,25 @@
     /*
     * Infinity loop the game
      */
-    function game_loop() {
-        while(true) {
-            console.log(GAMEFIELD);
+    async function game_loop() {
+        // while(true) {
+        //
+
+        await sleep(3000);
+            console.log("HEJ");
+            BUFFER.clearRect(0, 0, BUFFER.canvas.width, BUFFER.canvas.height);
+            CONTEXT.clearRect(0, 0, CONTEXT.canvas.width, CONTEXT.canvas.height);
+            BUFFER.beginPath();
+            CONTEXT.beginPath();
+            BUFFER.closePath();
+            CONTEXT.closePath();
             tick();
-            // add sleep
-        }
+            draw_gamefield();
+            BUFFER.fill();
+            CONTEXT.fill();
+            // resize();
+            console.log("OMPALOMP");
+        // }
     }
 
 
@@ -38,10 +58,122 @@
     /*
     * Calculates a tick.
     * During a tick, each cell is check against the rules.
-     */
-     function tick() {
+    */
+    function tick() {
+        let tick_changes = [];
+        for(let row=0; row<NR_ROWS; row++) {
+            for(let col=0; col<NR_ROWS; col++) {
+                let neighbor_value = get_neighborhood(row, col);
+                let rule = check_rules(GAMEFIELD[row, col], neighbor_value);
+                if (rule > 0) {
+                    tick_changes.push((row, col, rule));
+                }
+            }
+        }
+        activate_rules(tick_changes);
+    }
 
-     }
+
+
+    /*
+    * Activate rules for each changed cell on gamefield
+     */
+    function activate_rules(tick) {
+        let row_i = 0, col_i = 1, rule_i = 2;
+        tick.forEach(function(change) {
+            let rule = change[rule_i];
+            let row = change[row_i];
+            let col = change[col_i];
+
+            if (rule == 1) {
+                // Any live cell with fewer than two live neighbors dies, as if by under population.
+                GAMEFIELD[row][col] = DEAD_V;
+            }
+            else if (rule == 3) {
+                // Any live cell with more than three live neighbors dies, as if by overpopulation.
+                GAMEFIELD[row][col] = DEAD_V
+            }
+            else if (rule == 4) {
+                // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+                GAMEFIELD[row][col] = ALIVE_V
+            }
+        });
+    }
+
+
+
+    /*
+    * Check rule for cell
+     */
+    function check_rules(alive, n_value) {
+        if (alive && n_value < 2) { // 1
+            // Any live cell with fewer than two live neighbors dies, as if by under population.
+            return 1;
+        } else if (alive && 1 < n_value && n_value < 4) { // 2-3
+            // Any live cell with two or three live neighbors lives on to the next generation.
+            return -1;
+        } else if (alive && n_value > 3) { // > 3
+            // Any live cell with more than three live neighbors dies, as if by overpopulation.
+            return 3;
+        } else if ( !alive && n_value == 3) { // 3
+            // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+            return 4;
+        } else {
+            return -1
+        }
+    }
+
+
+
+    /*
+    * Calculate neighborhood value for a cell
+    */
+    function get_neighborhood(row, col) {
+        let last_rowI = NR_ROWS - 1;
+        let last_colI = NR_COLS - 1;
+
+        let above_ind = row == 0 ? last_rowI : row - 1;
+        let below_ind = row == last_rowI ? 0 : row + 1;
+        let left_ind = col == 0 ? last_colI : col - 1;
+        let right_ind = col == last_colI ? 0 : col + 1;
+
+        let above = 0, below = 0, left = 0,  right = 0;
+
+        if (col == 0) {
+            above = GAMEFIELD[above_ind].slice(col, col+2).reduce((a, b) => a + b) + GAMEFIELD[above_ind][left_ind];
+            below = GAMEFIELD[below_ind].slice(col, col+2).reduce((a, b) => a + b) + GAMEFIELD[below_ind][left_ind];
+        } else if (col == last_colI){
+            above = GAMEFIELD[above_ind].slice(col-1, col+1).reduce((a, b) => a + b) + GAMEFIELD[above_ind][right_ind];
+            below = GAMEFIELD[below_ind].slice(col-1, col+1).reduce((a, b) => a + b) + GAMEFIELD[below_ind][right_ind];
+        } else {
+            above = GAMEFIELD[above_ind].slice(col-1, col+2).reduce((a, b) => a + b);
+            below = GAMEFIELD[below_ind].slice(col-1, col+2).reduce((a, b) => a + b);
+        }
+        left  = GAMEFIELD[row][left_ind];
+        right  = GAMEFIELD[row][right_ind];
+
+        let neighbor_sum = above + below + left + right;
+        return neighbor_sum;
+    }
+
+
+
+    /*
+    * Check if range for column index will be out-of-bounds.
+    * Only applicable for over size not below size, -1.
+    */
+    function last_column(index) {
+        return index == NR_COLS-1;
+    }
+
+
+
+    /*
+    * calculate if row or column is out of bounds.
+    */
+    function calc_out_of_bounds(index, type_of) {
+        return type_of == "r" ? index % NR_ROWS : index % NR_COLS;
+    }
 
 
 
@@ -73,6 +205,7 @@
     * Got code from https://github.com/frankarendpoth/frankarendpoth.github.io/blob/master/content/pop-vlog/javascript/2017/012-tile-world/tile-world.js
      */
     function draw_gamefield() {
+
         for (let i = 0; i < GAMEFIELD.length; i ++) {
             for (let j = 0; j < GAMEFIELD[i].length; j ++) {
                 BUFFER.fillStyle = (GAMEFIELD[i][j] == 1)?"#000000":"#ffffff";
@@ -81,6 +214,8 @@
       }
 
       CONTEXT.drawImage(BUFFER.canvas, 0, 0, BUFFER.canvas.width, BUFFER.canvas.height, 0, 0, CONTEXT.canvas.width, CONTEXT.canvas.height);
+      window.requestAnimationFrame(game_loop);
+
     };
 
 
@@ -99,6 +234,16 @@
 
         draw_gamefield();
     };
+
+
+
+    /*
+    * Simple sleep function
+     */
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
 
 
     window.addEventListener("resize", resize, {passive:true});
