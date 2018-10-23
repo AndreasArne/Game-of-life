811a401e976e33d5135e04e600e4e313a768d30e
Andreas Arnesson <Andreas_osby@hotmail.com>
2018-10-23 12:34:31 +0200

    prepared for gui testing

---
+++ python/tests/test_gui.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Tests the functions used to for the gui in turtle_gui.py.
+"""
+from sys import argv, path
+import os
+import unittest
+path.insert(1, os.path.join(path[0], '..'))
+import turtle_gui
+import config
+
+class TestGui(unittest.TestCase):
+    """
+    Test the different functions used for in turtle_gui.py
+    """
+
+    def setUp(self):
+        config.NR_COLS = 20
+        config.NR_ROWS = 20
+
+    def assert_calc_bounds(self, type_of):
+        """
+        Asserts used to verify calc_bounds for both rows and columns
+        """
+        pass
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
