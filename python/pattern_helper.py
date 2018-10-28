c085bd23f91687fb28d8707140e05e98b0bb47b7
Andreas Arnesson <andreas_osby@hotmail.com>
2018-10-28 22:32:32 +0100

    started on formatting natrix in pattern_helper

--- python/pattern_helper.py
+++ python/pattern_helper.py
@@ -4,6 +4,7 @@
 Output a 2d json list to file to make it easier for creating patterns
 """
 import json
+from game import config
 
 def create_matrix(nr_rows, nr_cols):
     """
@@ -16,9 +17,21 @@ def write_to_file(matrix, name):
     """
     Use json to dump matrix to file
     """
-    file_path = "patterns/{}.json".format(name)
+    file_path = config.PATTERN_PATH + "/{}.json".format(name)
     with open(file_path, "w") as fh:
-        json.dump(matrix, fh, indent=4)
+        json.dump(matrix, fh)
+
+def format():
+    """
+    Format json with newlines
+    """
+    pass
+
+def insert_newline():
+    """
+    Insert newline after each list
+    """
+    pass
 
 def start():
     """
@@ -27,10 +40,10 @@ def start():
     rows = int(input("How many rows should there be? "))
     cols = int(input("How many columns should there be? "))
     filename = input("What is the name of the pattern? ")
-    
+
     matrix = create_matrix(rows, cols)
     write_to_file(matrix, filename)
-    print("File {} is now created in patterns/ for you to fill in a pattern!".format(filename))
+    print("File {} is now created in " + config.PATTERN_PATH + "/ for you to fill in a pattern!".format(filename))
 
 if __name__ == "__main__":
     start()
